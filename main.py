c220f97d5423c54674cd52dd5c84466fe0fdcb21
Kiril <kirilkostov853@gmail.com>
2025-01-27 13:48:54 +0200

    Gradio ui

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
+import gradio as gr
+
 from models.logistic_regression import model as lr_model
 from models.mlp import MLP
 from models.rnn_model import RNN
 from train import Trainer
+from models.gradio_fe import generate_and_predict
+
+
+
 
 # Constants
 MODEL_FILENAMES = {
@@ -15,8 +21,17 @@ def train_and_evaluate(model, model_name):
     trainer.train()
     trainer.evaluate()
 
+def start_fe():
+    interface = gr.Interface(fn=generate_and_predict, 
+                         inputs=gr.inputs.Slider(minimum=0, maximum=9, step=1, default=0, label="Input Digit (0-9)"), 
+                         outputs=gr.outputs.Textbox(label="Predictions"),
+                         live=True)
+
+    interface.launch()
+
+
 def main():
-    pass
+    start_fe()
     # Logistic Regression
     # train_and_evaluate(lr_model, MODEL_FILENAMES['logistic_regression'])
 
